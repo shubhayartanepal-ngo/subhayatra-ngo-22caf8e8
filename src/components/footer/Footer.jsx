1ed2221819893ee07fc99354e5bc1d31ff8c01a6
sujanstha10 <shresthasujan148@gmail.com>
2025-10-21 12:26:10 +0545

    footer links update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -98,37 +98,49 @@ const Footer = () => {
                       </li>
                       <li
                         className={
-                          location.pathname === "/about"
+                          location.pathname === "/ourteam"
                             ? "current-menu-item"
                             : ""
                         }
                       >
-                        <Link to="/about">About Us</Link>
+                        <Link to="/ourteam">Our Team</Link>
                       </li>
                       <li
                         className={
-                          location.pathname === "/about"
+                          location.pathname === "/gallery/photo" ||
+                          location.pathname === "/gallery/video"
                             ? "current-menu-item"
                             : ""
                         }
                       >
-                        <Link to="/about">Gallery</Link>
+                        <Link to="/gallery/photo">Gallery</Link>
                       </li>
                       <li
                         className={
-                          location.pathname === "/about"
+                          location.pathname === "/award"
                             ? "current-menu-item"
                             : ""
                         }
                       >
-                        <Link to="/about"> Achievements</Link>
+                        <Link to="/award">Achievements</Link>
                       </li>
                       <li
                         className={
-                          location.pathname === "/#" ? "current-menu-item" : ""
+                          location.pathname.includes("program")
+                            ? "current-menu-item"
+                            : ""
                         }
                       >
-                        <Link to="/about">Programs</Link>
+                        <Link to="/biodiversityprogram">Programs</Link>
+                      </li>
+                      <li
+                        className={
+                          location.pathname === "/newsevents"
+                            ? "current-menu-item"
+                            : ""
+                        }
+                      >
+                        <Link to="/newsevents">News & Events</Link>
                       </li>
                       <li
                         className={
